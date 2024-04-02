2cd2864d66d15ee7e7022c49992702c659e2f69c
s-agapov <online.server.log@gmail.com>
2024-04-02 09:16:44 +0400

    'Tink'

---
+++ TP/Work/Tink/Portfolio/single_shot.py
@@ -0,0 +1,159 @@
+"""
+    This code is an example of applying Trading Strategy for several Tickers.
+    The strategy in this code is for demonstration only purposes
+    - it outputs OHLCV values.
+    Author: Oleg Shpagin, my github: https://github.com/WISEPLAT
+"""
+
+import asyncio
+import logging
+
+from datetime import timedelta, datetime
+from typing import List, Optional
+
+from tinkoff.invest import AioRequestError, AsyncClient, CandleInterval, HistoricCandle
+from tinkoff.invest.async_services import AsyncServices
+from tinkoff.invest.utils import now
+
+import tink_port as tink
+
+TOKEN = 't.UFRJ8SC9hafVOhFxEUY7yf1wZ1gGhwJp-WCp9o4rnEChHWns0c3jQ21eQwoOW_RurFqeZpss2scJkmMQnomJ9g'
+
+logging.basicConfig(format="%(asctime)s %(levelname)s:%(message)s", level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+class PortfolioStrategy:
+    """This class is responsible for a strategy. You can put here
+    your methods for your strategy."""
+
+    def __init__(
+        self,
+        portfolio: List,
+        timeframe: CandleInterval,
+        days_back: int,
+        check_interval: int,
+        client: Optional[AsyncServices],
+    ):
+        self.account_id = None
+        self.portfolio = portfolio
+        self.timeframe = timeframe
+        self.days_back = days_back
+        self.check_interval = check_interval
+        self.client = client
+        self.ticker_base = []
+        self.candles: List[HistoricCandle] = []
+
+        self.ticker_base = tink.get_id_base(TOKEN)
+
+    async def get_historical_data(self):
+        """
+        Gets historical data for the instrument. Returns list of candles.
+        Requests all the candles of timeframe from days_back to now.
+
+        :return: list of HistoricCandle
+        """
+        logger.debug(
+            "Start getting historical data for %s days back from now. figi=%s",
+            self.days_back,
+        )
+
+        for ticker in portfolio:
+            figi = tink.ticker_to_figi(ticker, self.ticker_base)
+            async for candle in self.client.get_all_candles(
+                figi=figi,
+                from_=now() - timedelta(days=self.days_back),
+                to=now(),
+                interval=self.timeframe,
+            ):
+                if candle not in self.candles:
+                    if candle.is_complete:
+                        self.candles.append(candle)
+                        logger.debug("Found %s - figi=%s", candle, self.figi)
+
+
+
+    async def ensure_market_open(self):
+        """
+        Ensure that the market is open. Loop until the instrument is available.
+        :return: when instrument is available for trading
+        """
+        trading_status = await self.client.market_data.get_trading_status(
+            figi=self.figi
+        )
+        while not (
+            trading_status.market_order_available_flag
+            and trading_status.api_trade_available_flag
+        ):
+            logger.debug("Waiting for the market to open. figi=%s", self.figi)
+            await asyncio.sleep(60)
+            trading_status = await self.client.market_data.get_trading_status(
+                figi=self.figi
+            )
+
+    async def main_cycle(self):
+        """Main cycle for live strategy."""
+        try:
+#                await self.ensure_market_open()
+            await self.get_historical_data()
+
+            print("Live mode:", datetime.now())
+            logger.debug(
+                "- live mode: run some strategy code to buy or sell - figi=%s",
+                self.figi,
+            )
+
+        except AioRequestError as are:
+            logger.error("Client error %s", are)
+
+        await asyncio.sleep(self.check_interval)
+
+    async def start(self):
+        """Strategy starts from this function."""
+        if self.account_id is None:
+            try:
+                self.account_id = (
+                    (await self.client.users.get_accounts()).accounts.pop().id
+                )
+            except AioRequestError as are:
+                logger.error("Error taking account id. Stopping strategy. %s", are)
+                return
+        await self.main_cycle()
+
+
+async def run_strategy(portfolio, timeframe, days_back, check_interval):
+    """From this function we are starting
+    strategy for every ticker from portfolio.
+    """
+    async with AsyncClient(token=TOKEN, app_name="TinkoffApp") as client:
+        strategy_tasks = []
+        strategy = PortfolioStrategy(
+            portfolio,
+            timeframe=timeframe,
+            days_back=days_back,
+            check_interval=check_interval,
+            client=client,
+        )
+        strategy_tasks.append(asyncio.create_task(strategy.start()))
+        await asyncio.gather(*strategy_tasks)
+
+
+if __name__ == "__main__":
+    sber_figi = "BBG004730N88"
+    vtbr_figi = "BBG004730ZJ9"
+    portfolio = {sber_figi, vtbr_figi}
+    timeframe = CandleInterval.CANDLE_INTERVAL_1_MIN
+    days_back = 1
+    check_interval = 60  # seconds to check interval for new completed candle
+
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(
+        run_strategy(
+            portfolio=portfolio,
+            timeframe=timeframe,
+            days_back=days_back,
+            check_interval=check_interval,
+        )
+    )
+    loop.run_until_complete(task)
+    print("All done")
